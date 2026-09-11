cd4f2968424acf5bf5001c795da8a4cd5aed6e9e
José Andrés Lorenzo Robles <joseandres.l.r_15@hotmail.com>
2026-09-11 17:29:12 +0200

    chore: migra a Angular 21 y habilita SSR con prerender

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,10 @@
 import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter, withViewTransitions, withInMemoryScrolling } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { APP_ENVIRONMENT, appEnvironment } from './core/app-environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +15,8 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
     ),
     provideAnimationsAsync(),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    { provide: APP_ENVIRONMENT, useValue: appEnvironment },
   ],
 };
